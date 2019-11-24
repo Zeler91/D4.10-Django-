8ff930d1798c91a0124614514ad848d17d8dfd01
Zeler91 <51795219+Zeler91@users.noreply.github.com>
2019-11-24 16:44:03 +0300

    Initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\studing full stack\practic\backend\django\d3_10\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ my_site/p_library/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+  
+class Author(models.Model):  
+    full_name = models.TextField()  
+    birth_year = models.SmallIntegerField()  
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return "{self.full_name}".format(self = self) 
+
+class Redaction(models.Model):
+    name = models.TextField()
+    creation_year = models.SmallIntegerField()
+    description = models.TextField()
+    
+    def __str__(self):
+        return "{self.name}".format(self = self) 
+
+class Book(models.Model):  
+    ISBN = models.CharField(max_length=13)  
+    title = models.TextField()  
+    description = models.TextField()  
+    year_release = models.SmallIntegerField()  
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    redaction = models.ForeignKey(Redaction, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    copy_count = models.SmallIntegerField(default = 1)
+
+    def __str__(self):
+        return "{self.title}, ".format(self = self) 
